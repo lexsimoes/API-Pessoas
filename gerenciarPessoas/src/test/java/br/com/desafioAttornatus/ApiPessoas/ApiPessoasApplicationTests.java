d9cec7dbe4369ccda54c93fb2851cf0654957a82
Alexandre Simoes <lexsimoes@yahoo.com>
2023-01-16 15:08:02 -0300

    conf iniciais

---
+++ gerenciarPessoas/src/test/java/br/com/desafioAttornatus/ApiPessoas/ApiPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafioAttornatus.ApiPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
